a0ba0c4f10d65540416dce085d7bfc5e8264a0e8
antran1245 <antran1245@gmail.com>
2022-12-08 12:18:32 -0800

    setting global styles and adding tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{tsx,ts}"],
   theme: {
+    colors: {
+      'D87D4A': '#D87D4A',
+      '#101010': '#101010',
+      'F1F1F1': '#F1F1F1',
+      'FAFAFA': '#FAFAFA',
+      'FBAF85': '#FBAF85',
+      'FFFFFF': '#FFFFFF',
+      '000000': '#000000',
+      'CD2C2C': '#CD2C2C',
+      'CFCFCF': '#CFCFCF'
+    },
     extend: {},
   },
   plugins: [],
