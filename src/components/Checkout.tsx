8774e077fa3a731efa9320dda93f08fe898c23bd
antran1245 <antran1245@gmail.com>
2022-12-18 18:24:31 -0800

    checkout form tablet and mobile style

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -22,22 +22,24 @@ export default function Checkout({ setOpenCart }: CheckoutProps) {
           </span>
         </p>
         <div className="mx-auto w-[70%] pb-[141px] xl:w-[80%] lg:w-[90%]">
-          <form className="w-[70%] rounded-[8px] bg-FFFFFF p-[48px]">
+          <form className="w-[70%] rounded-[8px] bg-FFFFFF p-[48px] md:w-full md:py-[30px] md:px-[27px] sm:p-[24px]">
             <p className="heading3 mb-[41px]">CHECKOUT</p>
             {/* 
               Billing Details
             */}
             <p className="subtitle mb-[16px] text-D87D4A">BILLING DETAILS</p>
-            <div className="mb-[53px] flex flex-wrap">
-              <div className="form-group mr-[16px] mb-[24px] w-[48%]">
-                <label>Name</label>
-                <input type="text" placeholder="Name" />
+            <div className="mb-[53px] flex flex-col sm:mb-[32px]">
+              <div className="flex w-full justify-between sm:flex-col">
+                <div className="form-group mb-[24px] w-[49%] sm:w-full">
+                  <label>Name</label>
+                  <input type="text" placeholder="Name" />
+                </div>
+                <div className="form-group mb-[24px] w-[49%] sm:w-full">
+                  <label>Email Address</label>
+                  <input type="text" placeholder="Email Address" />
+                </div>
               </div>
-              <div className="form-group mr-[16px] mb-[24px] w-[48%]">
-                <label>Email Address</label>
-                <input type="text" placeholder="Email Address" />
-              </div>
-              <div className="form-group mr-[16px] mb-[24px] w-[48%]">
+              <div className="form-group mb-[24px] w-[49%] sm:w-full">
                 <label>Phone Number</label>
                 <input type="text" placeholder="Phone Number" />
               </div>
@@ -46,20 +48,22 @@ export default function Checkout({ setOpenCart }: CheckoutProps) {
               Shipping info
             */}
             <p className="subtitle mb-[16px] text-D87D4A">SHIPPING INFO</p>
-            <div className="form-group mb-[24px] w-[100%]  pr-[16px]">
+            <div className="form-group mb-[24px] w-[100%]">
               <label>Address</label>
               <input type="text" placeholder="Address" />
             </div>
-            <div className="mb-[53px] flex flex-wrap">
-              <div className="form-group mr-[16px] mb-[24px] w-[48%]">
-                <label>ZIP Code</label>
-                <input type="text" placeholder="ZIP Code" />
+            <div className="mb-[53px] flex flex-wrap sm:mb-[32px]">
+              <div className="flex w-full justify-between sm:flex-col">
+                <div className="form-group mb-[24px] w-[49%] sm:w-full">
+                  <label>ZIP Code</label>
+                  <input type="text" placeholder="ZIP Code" />
+                </div>
+                <div className="form-group mb-[24px] w-[49%] sm:w-full">
+                  <label>City</label>
+                  <input type="text" placeholder="City" />
+                </div>
               </div>
-              <div className="form-group mr-[16px] mb-[24px] w-[48%]">
-                <label>City</label>
-                <input type="text" placeholder="City" />
-              </div>
-              <div className="form-group mr-[16px] mb-[24px] w-[48%]">
+              <div className="form-group mb-[24px] w-[49%] sm:w-full">
                 <label>Country</label>
                 <input type="text" placeholder="Country" />
               </div>
@@ -68,28 +72,28 @@ export default function Checkout({ setOpenCart }: CheckoutProps) {
               Payment Detail
             */}
             <p className="subtitle mb-[16px] text-D87D4A">PAYMENT DETAILS</p>
-            <div className="flex">
-              <p className="w-[50%] text-[12px] font-[700] leading-[16px] tracking-[-0.21px]">
+            <div className="flex justify-between sm:flex-col">
+              <p className="w-[49%] text-[12px] font-[700] leading-[16px] tracking-[-0.21px]">
                 Payment Method
               </p>
-              <div className="w-[50%]">
-                <div className="form-group-radio flex w-[96%] items-center">
+              <div className="w-[49%] sm:mt-[17px] sm:w-full">
+                <div className="form-group-radio flex w-[100%] items-center sm:mb-[16px] sm:w-full">
                   <input type="radio" name="payment-option" />
-                  <label>e-Money</label>
+                  <label className="ml-[16px]">e-Money</label>
                 </div>
-                <div className="form-group-radio flex w-[96%] items-center">
+                <div className="form-group-radio flex w-[100%] items-center sm:w-full">
                   <input type="radio" name="payment-option" />
-                  <label>Cash on Delivery</label>
+                  <label className="ml-[16px]">Cash on Delivery</label>
                 </div>
               </div>
             </div>
-            <div className="flex">
-              <div className="form-group mr-[16px] w-[48%]">
-                <label>e-Money Number</label>
+            <div className="flex justify-between sm:flex-col">
+              <div className="form-group w-[49%] sm:mt-[32px] sm:w-full">
+                <label className="sm:mb-[9px]">e-Money Number</label>
                 <input type="text" placeholder="e-Money Number" />
               </div>
-              <div className="form-group w-[48%]">
-                <label>e-Money PIN</label>
+              <div className="form-group w-[49%] sm:mt-[24px] sm:w-full">
+                <label className="sm:mb-[9px]">e-Money PIN</label>
                 <input type="text" placeholder="PIN Number" />
               </div>
             </div>
