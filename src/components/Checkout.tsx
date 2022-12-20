0b168b02444442a376164ef5ce3cfbf36723e086
antran1245 <antran1245@gmail.com>
2022-12-20 15:21:46 -0800

    checkout summary showing all items

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { CheckoutProps } from "../interfaces/CheckoutInterface";
 import "../sass/checkout.scss";
+import Form from "./CheckoutComponents/Form";
+import Summary from "./CheckoutComponents/Summary";
 
 export default function Checkout({ setOpenCart }: CheckoutProps) {
   const navigate = useNavigate();
@@ -22,115 +24,10 @@ export default function Checkout({ setOpenCart }: CheckoutProps) {
         </p>
         <div className="mx-auto flex w-[80%] justify-between pb-[141px] xl:w-[80%] lg:w-[90%] md:flex-col">
           {/* Billing, Shipping, Payment form */}
-          <form className="w-[68%] rounded-[8px] bg-FFFFFF p-[48px] md:w-full md:py-[30px] md:px-[27px] sm:p-[24px]">
-            <p className="heading3 mb-[41px]">CHECKOUT</p>
-            {/* 
-              Billing Details
-            */}
-            <p className="subtitle mb-[16px] text-D87D4A">BILLING DETAILS</p>
-            <div className="mb-[53px] flex flex-col sm:mb-[32px]">
-              <div className="flex w-full justify-between sm:flex-col">
-                <div className="form-group mb-[24px] w-[49%] sm:w-full">
-                  <label>Name</label>
-                  <input type="text" placeholder="Name" />
-                </div>
-                <div className="form-group mb-[24px] w-[49%] sm:w-full">
-                  <label>Email Address</label>
-                  <input type="text" placeholder="Email Address" />
-                </div>
-              </div>
-              <div className="form-group mb-[24px] w-[49%] sm:w-full">
-                <label>Phone Number</label>
-                <input type="text" placeholder="Phone Number" />
-              </div>
-            </div>
-            {/* 
-              Shipping info
-            */}
-            <p className="subtitle mb-[16px] text-D87D4A">SHIPPING INFO</p>
-            <div className="form-group mb-[24px] w-[100%]">
-              <label>Address</label>
-              <input type="text" placeholder="Address" />
-            </div>
-            <div className="mb-[53px] flex flex-wrap sm:mb-[32px]">
-              <div className="flex w-full justify-between sm:flex-col">
-                <div className="form-group mb-[24px] w-[49%] sm:w-full">
-                  <label>ZIP Code</label>
-                  <input type="text" placeholder="ZIP Code" />
-                </div>
-                <div className="form-group mb-[24px] w-[49%] sm:w-full">
-                  <label>City</label>
-                  <input type="text" placeholder="City" />
-                </div>
-              </div>
-              <div className="form-group mb-[24px] w-[49%] sm:w-full">
-                <label>Country</label>
-                <input type="text" placeholder="Country" />
-              </div>
-            </div>
-            {/* 
-              Payment Detail
-            */}
-            <p className="subtitle mb-[16px] text-D87D4A">PAYMENT DETAILS</p>
-            <div className="flex justify-between sm:flex-col">
-              <p className="w-[49%] text-[12px] font-[700] leading-[16px] tracking-[-0.21px]">
-                Payment Method
-              </p>
-              <div className="w-[49%] sm:mt-[17px] sm:w-full">
-                <div className="form-group-radio flex w-[100%] items-center sm:mb-[16px] sm:w-full">
-                  <input type="radio" name="payment-option" />
-                  <label className="ml-[16px]">e-Money</label>
-                </div>
-                <div className="form-group-radio flex w-[100%] items-center sm:w-full">
-                  <input type="radio" name="payment-option" />
-                  <label className="ml-[16px]">Cash on Delivery</label>
-                </div>
-              </div>
-            </div>
-            <div className="flex justify-between sm:flex-col">
-              <div className="form-group w-[49%] sm:mt-[32px] sm:w-full">
-                <label className="sm:mb-[9px]">e-Money Number</label>
-                <input type="text" placeholder="e-Money Number" />
-              </div>
-              <div className="form-group w-[49%] sm:mt-[24px] sm:w-full">
-                <label className="sm:mb-[9px]">e-Money PIN</label>
-                <input type="text" placeholder="PIN Number" />
-              </div>
-            </div>
-          </form>
+          <Form />
 
           {/* Summary */}
-          <div className="h-fit w-[30%] rounded-[8px] bg-FFFFFF py-[32px] px-[33px] md:mt-[32px] md:w-full">
-            <p className="heading6">SUMMARY</p>
-            {/* Put the items here */}
-            <div className="mb-[8px] flex items-center justify-between">
-              <p className="body text-000000/50">TOTAL</p>
-              <p className="text-[18px] font-[700] leading-[25px] text-000000">
-                $0
-              </p>
-            </div>
-            <div className="mb-[8px] flex items-center justify-between">
-              <p className="body text-000000/50">SHIPPING</p>
-              <p className="text-[18px] font-[700] leading-[25px] text-000000">
-                $0
-              </p>
-            </div>
-            <div className="mb-[24px] flex items-center justify-between">
-              <p className="body text-000000/50">VAT &#x28;INCLUDED&#x29;</p>
-              <p className="text-[18px] font-[700] leading-[25px] text-000000">
-                $0
-              </p>
-            </div>
-            <div className="mb-[32px] flex items-center justify-between">
-              <p className="body text-000000/50">GRAND TOTAL</p>
-              <p className="text-[18px] font-[700] leading-[25px] text-000000">
-                $0
-              </p>
-            </div>
-            <button className="button-1 w-full text-[13px] font-[700] leading-[18px] tracking-[1px]">
-              CONTINUE
-            </button>
-          </div>
+          <Summary />
         </div>
       </div>
     </main>
---
+++ src/components/CheckoutComponents/Summary.tsx
@@ -0,0 +1,77 @@
+import { useContext } from "react";
+import ShopContext from "../context/ShopContext";
+
+export default function Summary() {
+  const context = useContext(ShopContext);
+  const { cart } = context;
+  const total = cart.reduce(
+    (total, current) => total + current.cartQuantity * current.price,
+    0
+  );
+
+  return (
+    <div className="h-fit w-[30%] rounded-[8px] bg-FFFFFF py-[32px] px-[33px] md:mt-[32px] md:w-full">
+      <p className="heading6 mb-[31px]">SUMMARY</p>
+      {/* Put the items here */}
+      {cart.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="mb-[24px] flex items-center justify-between"
+          >
+            <div className="flex items-center">
+              <img
+                src={require(`../../assets${item.image.desktop.substring(
+                  item.image.desktop.indexOf(
+                    "/",
+                    item.image.desktop.indexOf("/") + 1
+                  ),
+                  item.image.desktop.length
+                )}`)}
+                alt={item.slug}
+                className="h-[64px] w-[64px] rounded-[8px]"
+              />
+              <div className="ml-[16px] font-[700] leading-[25px]">
+                <p className="text-[15px]">
+                  {item.name.replace(
+                    /Wireless|Earphones|Headphones|Speaker/g,
+                    ""
+                  )}
+                </p>
+                <p className="test-[14px] text-000000/50">${item.price}</p>
+              </div>
+            </div>
+            <p className="body font-500 text-000000/50">x{item.cartQuantity}</p>
+          </div>
+        );
+      })}
+      <div className="mb-[8px] mt-[32px] flex items-center justify-between">
+        <p className="body text-000000/50">TOTAL</p>
+        <p className="text-[18px] font-[700] leading-[25px] text-000000">
+          ${total}
+        </p>
+      </div>
+      <div className="mb-[8px] flex items-center justify-between">
+        <p className="body text-000000/50">SHIPPING</p>
+        <p className="text-[18px] font-[700] leading-[25px] text-000000">
+          ${cart.length > 0 ? "50" : "0"}
+        </p>
+      </div>
+      <div className="mb-[24px] flex items-center justify-between">
+        <p className="body text-000000/50">VAT &#x28;INCLUDED&#x29;</p>
+        <p className="text-[18px] font-[700] leading-[25px] text-000000">
+          ${Math.floor(total * 0.2)}
+        </p>
+      </div>
+      <div className="mb-[32px] flex items-center justify-between">
+        <p className="body text-000000/50">GRAND TOTAL</p>
+        <p className="text-[18px] font-[700] leading-[25px] text-000000">
+          ${total + Math.floor(total * 0.2) + (cart.length > 0 ? 50 : 0)}
+        </p>
+      </div>
+      <button className="button-1 w-full text-[13px] font-[700] leading-[18px] tracking-[1px]">
+        CONTINUE & PAY
+      </button>
+    </div>
+  );
+}
